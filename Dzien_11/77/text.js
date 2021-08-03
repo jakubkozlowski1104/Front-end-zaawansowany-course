835f3b2c13d90d9afd2d6c3d7a853c3880fe2077
user.email <jakub.kozlowski1104@gmail.com>
2021-08-03 21:33:47 +0200

    add function

--- Dzien_11/77/text.js
+++ Dzien_11/77/text.js
@@ -1,2 +1,14 @@
 
 const btnIncrease = document.querySelector('bigger');
+const btnDecrease = document.querySelector('smaller');
+const text = document.querySelector('p');
+
+let textSize = 16;
+
+
+function textIncrease() {
+    text.style.fontSize = "30px";
+}
+
+
+btnIncrease.addEventListener('click', textIncrease);
