d8fc7e0e147f3e3e7d0adeb23dedb35805fb8900
user.email <jakub.kozlowski1104@gmail.com>
2021-08-04 20:26:30 +0200

    add toogle

--- Dzien_11/78/script.js
+++ Dzien_11/78/script.js
@@ -1,1 +1,8 @@
-const firstDi = document.querySelector('.s1');
+const firstDiv = document.querySelector('.s1');
+const secoundDiv = document.querySelector('.s2');
+
+function changeColor() {
+    firstDiv.classList.toggle('red');
+    secoundDiv.classList.toggle('red');
+}
+firstDiv.addEventListener('click', changeColor);
