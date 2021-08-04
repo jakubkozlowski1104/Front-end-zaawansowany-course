cca238469302acd1cadb54fa2893450b4c6fe104
user.email <jakub.kozlowski1104@gmail.com>
2021-08-04 20:30:48 +0200

    78 final commit

--- Dzien_11/78/script.js
+++ Dzien_11/78/script.js
@@ -2,7 +2,7 @@ const firstDiv = document.querySelector('.s1');
 const secoundDiv = document.querySelector('.s2');
 
 function changeColor() {
-    firstDiv.classList.toggle('red');
-    secoundDiv.classList.toggle('red');
+    this.classList.toggle('red');
 }
 firstDiv.addEventListener('click', changeColor);
+secoundDiv.addEventListener('click', changeColor);
