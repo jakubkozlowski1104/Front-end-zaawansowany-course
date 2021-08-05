c848fb1654ce7a7427aa6c4c593e9d99aaa84bcb
user.email <jakub.kozlowski1104@gmail.com>
2021-08-05 22:04:33 +0200

    add function rotate

---
+++ Dzien_11/79/rotate.js
@@ -0,0 +1,14 @@
+  // parametry główne. Za pomocą zmiennych stwórz początkową wartość rotate (zero) i wartość o którą będziesz zmieniał stopnie (o 10 stopnii).
+
+  // pobieramy elementy - pobierz przycisk i div
+
+  // podpinamy nasłuchiwanie i umeiszczamy w nim funkcje anonimową. Pamiętaj zmieniamy własciwość transform. Za każdym razie obrót zwiększa się od 10 deg. 
+
+  //Bonus dodaj właściwość transition dla div określoną na .5s.
+
+const btnRotate = document.querySelector('button');
+let square = document.querySelector('div');
+
+let rotate = 0;
+let deg = 0
+
