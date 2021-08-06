bea97c5c1d944d0ca134f2ebc2c47876c5414323
user.email <jakub.kozlowski1104@gmail.com>
2021-08-06 23:13:50 +0200

    add toggle class orange

--- Dzien_12/85/exercise.js
+++ Dzien_12/85/exercise.js
@@ -1,5 +1,10 @@
 $('.interface').on('click', function() {
-    console.log('dziala');
     console.log(this);
+    console.log($(this));
+    console.log($(this).attr("class"));
     
+    if($(this).hasClass('orange')) {
+        console.log('prawda w pomaranczowy');
+        $('body').toggleClass('orange');
+    }
 })
